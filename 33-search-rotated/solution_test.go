20de3e53f5f7c42c0a2b6fc160030a097dc3d221
James Moessis <james.moessis1@gmail.com>
2024-03-17 20:17:06 +1100

    uncomment tests

--- 33-search-rotated/solution_test.go
+++ 33-search-rotated/solution_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	// assert.Equal(t, 1, search([]int{2, 3, 4, 5, 0, 1}, 3))
-	// assert.Equal(t, 4, search([]int{2, 3, 4, 5, 0, 1}, 0))
-	// assert.Equal(t, 0, search([]int{6, 7, 8, 1, 2, 3, 4, 5}, 6))
+	assert.Equal(t, 1, search([]int{2, 3, 4, 5, 0, 1}, 3))
+	assert.Equal(t, 4, search([]int{2, 3, 4, 5, 0, 1}, 0))
+	assert.Equal(t, 0, search([]int{6, 7, 8, 1, 2, 3, 4, 5}, 6))
 	assert.Equal(t, 0, search([]int{1, 3, 5}, 1))
 }
