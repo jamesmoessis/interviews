c048802502df23ce52879b061a9198d65beb97fc
James Moessis <jmoessis@atlassian.com>
2024-03-20 23:06:46 +1100

    couple more tests

--- 15-3sum/solution_test.go
+++ 15-3sum/solution_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestAll(t *testing.T) {
-	//assert.Equal(twoSum([]int{1}, 1))
 	assert.Equal(t, [][2]int{{1, -1}}, twoSum([]int{1, -1}, 0))
-	//twoSum([]int{1}, 1)
+	assert.Equal(t, [][2]int{{1, -1}, {0, 0}}, twoSum([]int{1, -1, 0, 0}, 0))
+	assert.Equal(t, [][2]int{{1, 1}, {-1, 3}}, twoSum([]int{1, 1, -1, 3}, 2))
 }
