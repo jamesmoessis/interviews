6ddb3e64ebadb936b2335f7beb93b304e9264d5c
James Moessis <jmoessis@atlassian.com>
2024-03-21 22:54:52 +1100

    slow asf 3sum

--- 15-3sum/solution.go
+++ 15-3sum/solution.go
@@ -1,6 +1,46 @@
 package main
 
+import "slices"
+
+// threeSum solves the 3-sum problem by reducing it to n 2-sum problems
+// So if we have an O(n) 2-sum we can get an O(n^2) 3-sum.
+func threeSum(nums []int) [][]int {
+	m := make(map[int][]int)
+	for i, num := range nums {
+		indexes, ok := m[num]
+		if !ok {
+			m[num] = []int{i}
+		} else {
+			m[num] = append(indexes, i)
+		}
+	}
+
+	solSet := make(map[[3]int]bool)
+
+	for i, num := range nums {
+		target := -1 * num
+		twoSums := twoSum(removeIndex(nums, i), target)
+		for _, pair := range twoSums {
+			arr := [3]int{num, pair[0], pair[1]}
+			slices.Sort(arr[:])
+			solSet[arr] = true
+		}
+	}
+	sol := make([][]int, 0, len(solSet))
+	for k := range solSet {
+		sol = append(sol, k[:])
+	}
+	return sol
+}
+
+func removeIndex(s []int, index int) []int {
+	ret := make([]int, 0, len(s)-1)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
 // I'm making this harder by returning all pairs.
+// Not quite O(n), actually o(n^2) -- is it possible to get this to O(n)?
 func twoSum(nums []int, target int) [][2]int {
 	n := len(nums)
 	m := make(map[int][]int, n*2)
--- 15-3sum/solution_test.go
+++ 15-3sum/solution_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,14 @@ func TestHard(t *testing.T) {
 
 func TestEasy(t *testing.T) {
 	assert.Equal(t, []int{1, 2}, twoSumEasy([]int{3, 2, 4}, 6))
+}
+
+func Test3Sum(t *testing.T) {
+	assert.ElementsMatch(t, [][]int{{-1, -1, 2}, {-1, 0, 1}}, threeSum([]int{-1, 0, 1, 2, -1, -4}))
+}
+
+func TestSortingArray(t *testing.T) {
+	arr := [3]int{3, 2, 1}
+	slices.Sort(arr[:])
+	assert.Equal(t, [3]int{1, 2, 3}, arr)
 }
