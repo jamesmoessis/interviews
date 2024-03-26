fb91d2a7da573cab0943dfa5074f3afbd47343e6
James Moessis <jmoessis@atlassian.com>
2024-03-26 22:26:19 +1100

    solution for 22

--- 22-generate-parentheses/solution.py
+++ 22-generate-parentheses/solution.py
@@ -1,41 +1,23 @@
-# ()
-# ()()     (())
-# 
-# ((()))   (())()   ()(()) ()()() (()())
-# (((()))) ((()))() (())(()) ()((()))
-
-
-# To get n, we take each solution to n-1 and:
-# 1. put parenthese around it 
-# 2. Put to the left 
-# 3. put to the right
-# 4. at the very end, de-duplicate everything
-
-def generateSet(n): 
-    if n == 1:
-        return ["()"]
-    subsol = generateSet(n-1)
-    while len(subsol[0]) == (n-1)*2:
-        sol = subsol.pop(0)
-        subsol.append('()' + sol)
-        subsol.append(sol + '()')
-        subsol.append('(' + sol + ')')
-    return subsol
-
+def rec(s, n: int):
+    # L + R
+    if len(s) == n*2:
+        return [s]
+    lefts = 0
+    rights = 0
+    for c in s:
+        if c == '(':
+            lefts += 1
+        elif c == ')':
+            rights += 1
+    if rights > lefts or lefts > n:
+        return []
+    sol = []
+    if lefts+1 <= n:
+        sol += rec(s+'(', n)
+    if rights < lefts:
+        sol += rec(s+')', n)
+    return sol
 
 class Solution:
     def generateParenthesis(self, n: int):
-        return list(set(generateSet(n)))
-    
-if __name__ == '__main__':
-    print(Solution().generateParenthesis(3))
-    print(sorted(Solution().generateParenthesis(4)))
-
-#["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
-#["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
-# "(())(())(())"
-# 1 -> 1 
-# 2 -> 2
-# 3 -> 5  = (2 * 3) - 1 
-# 4 -> 14 = (5 * 3) - 1
-# 5 -> 42 = (14*3)
+        return rec('(', n)
