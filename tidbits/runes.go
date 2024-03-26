5604631d58d139744d02ee8d10f7de54c38767f3
James Moessis <jmoessis@atlassian.com>
2024-03-26 22:25:35 +1100

    playing around and committing it

---
+++ tidbits/runes.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//someStr := "-123abc"
+	//if someStr[0] == '-' {
+	//	print("hi")
+	//}
+	//for _, char := range someStr {
+	//	numVal := char - 48
+	//	fmt.Print(numVal)
+	//}
+	var a int32 = 2147483647
+	a++
+	fmt.Print(a)
+}
