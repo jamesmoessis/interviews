d1ac0d8991dce098f884eefeb86640e29111e64c
James Moessis <jmoessis@atlassian.com>
2024-04-26 11:56:11 +1000

    some notes about two tiktok interviews, todo implement

---
+++ tiktok/solution_2.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+The question was: find sqrt(n) to 5 decimal places without using math library.
+todo implement.
+*/
+
+func Sqrt(n float64) float64 {
+	return 0
+}
---
+++ tiktok/solution_3.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+The question was: You get an array with items either 0,1,2.
+Sort the array.
+You can do in O(n) with a map.
+You can do even faster by swapping the members directly, which is what they wanted me to do.
+todo implement that
+*/
+
+func sort(a []int) []int {
+	return make([]int, 0)
+}
